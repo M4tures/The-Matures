07a18801ac685e102b6293339eba12f38dcc22a0
M4tures <beginingtoo@gmail.com>
2026-09-14 15:49:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+console.log('The Matures loaded');
+
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector("#menu");
+
+
+hamburger.addEventListener("click", function(){
+
+    menu.classList.toggle("active");
+
+    console.log(menu.classList);
+
+});
+
+
+
+const menuLinks = document.querySelectorAll("#menu a");
+
+
+menuLinks.forEach(function(link){
+
+    link.addEventListener("click", function(){
+
+        menu.classList.remove("active");
+
+    });
+
+});
+
+
+
+window.addEventListener("resize", function(){
+
+    if(window.innerWidth > 700){
+
+        menu.classList.remove("active");
+
+    }
+
+});
+document.addEventListener("click", function(event){
+
+    if(
+        !menu.contains(event.target) &&
+        !hamburger.contains(event.target)
+    ){
+
+        menu.classList.remove("active");
+
+    }
+
+});
+const reveals = document.querySelectorAll(".reveal");
+
+
+window.addEventListener("scroll", function(){
+
+    reveals.forEach(function(element){
+
+        const windowHeight = window.innerHeight;
+
+        const elementTop = element.getBoundingClientRect().top;
+
+        const revealPoint = 100;
+
+
+        if(elementTop < windowHeight - revealPoint){
+
+            element.classList.add("active");
+
+        }
+
+    });
+
+});
+const header = document.querySelector(".header");
+
+
+window.addEventListener("scroll", function(){
+
+    if(window.scrollY > 50){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
